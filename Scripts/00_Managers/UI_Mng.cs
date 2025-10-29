8ff6c2497e2395a83cfb243f539db292aeb4c447
Minssuy <rlaalstjd6302@naver.com>
2025-10-29 13:31:12 +0900

    feat: Fade 및 SetAlpha 함수 오버로드 추가

---
+++ Scripts/00_Managers/UI_Mng.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Mng
+{ 
+    public IEnumerator Fade(Image image, float duration, float targetAlpha)
+    {
+        float startAlpha = image.color.a;
+        float time = 0f;
+        while (time < duration)
+        {
+            time += Time.deltaTime;
+            float currentAlpha = Mathf.Lerp(startAlpha, targetAlpha, time / duration);
+            SetAlpha(image, currentAlpha);
+            yield return null;
+        }
+        SetAlpha(image, targetAlpha);
+    }
+    
+    public IEnumerator Fade(SpriteRenderer spriterenderer, float duration, float targetAlpha)
+    {
+        float startAlpha = spriterenderer.color.a;
+        float time = 0f;
+        while (time < duration)
+        {
+            time += Time.deltaTime;
+            float currentAlpha = Mathf.Lerp(startAlpha, targetAlpha, time / duration);
+            SetAlpha(spriterenderer, currentAlpha);
+            yield return null;
+        }
+        SetAlpha(spriterenderer, targetAlpha);
+    }
+
+    public void SetAlpha(Image image, float alpha)
+    {
+        Color color = image.color;
+        color.a = alpha;
+        image.color = color;
+    }
+
+    public void SetAlpha(SpriteRenderer spriterenderer, float alpha)
+    {
+        Color color = spriterenderer.color;
+        color.a = alpha;
+        spriterenderer.color = color;
+    }
+}
